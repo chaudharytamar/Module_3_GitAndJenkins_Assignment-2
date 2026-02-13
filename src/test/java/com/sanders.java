2bdd42cdf833ec656e52e3e362146a16e03602d5
manvendra <chaudharytamar@gmail.com>
2026-02-13 20:53:56 +0530

    Test Case Commit

---
+++ src/test/java/com/sanders.java
@@ -0,0 +1,13 @@
+package com.sanders;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+        int sum = 5 + 3;
+        assertEquals(8, sum);
+    }
+}
